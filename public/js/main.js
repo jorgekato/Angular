09f0971596124266e5fbfbacd3c30b4bbf22df23
Jorge Kato <katojorge@hotmail.com>
2018-12-10 21:10:44 -0200

    Exibindo fotos

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
-angular.module('alurapic',[]);
+angular.module('alurapic',['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+.config(function($routeProvider, $locationProvider) {
+
+	// ativa o módulo do HTML5
+	$locationProvider.html5Mode(true);
+
+	$routeProvider.when('/fotos', {
+		templateUrl: 'partials/principal.html',
+		controller: 'FotosController'
+	});
+
+	$routeProvider.when('/fotos/novo', {
+		templateUrl: 'partials/foto.html',
+		
+	});
+
+	$routeProvider.otherwise({redirectTo: '/fotos' })
+
+});//depende do modulo minhasDiretivas. Pode ser inserido mais modulos.
